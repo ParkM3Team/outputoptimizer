0af11f714fe953f763addec11bf195ca3428e383
aaron <aaron@aaron-pc.home>
2015-12-13 20:18:49 -0500

    Basic Code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,47 @@
 
 public class Main {
 
+	public static final int MINVALUE = 5;
+	public static final int STARTVALUE = 100;
+	
+	public static void main(String[] args){
+		int bestSolution = 0;
+		boolean foundSolution = false;
+		int potentialSolution = STARTVALUE;
+		int result = someFunction(potentialSolution);
+		int previousSolution;
+		int previousResult;
+		if(result<MINVALUE){
+			System.out.println("NO POSSIBLE ANSWER");
+		}else{
+			previousSolution = potentialSolution;
+			previousResult = result;
+			potentialSolution--;
+			while(!foundSolution){
+				result = someFunction(potentialSolution);
+				if(potentialSolution<MINVALUE){
+					//Gone to far, last solution is best
+					System.out.println("BEST SOLUTION: "+previousSolution);
+					foundSolution = true;
+				}else{
+					//This is also a solution, we can keep going!
+					previousSolution = potentialSolution;
+					previousResult = result;
+					potentialSolution--;
+				}
+			}
+		}
+	
+		
+		
+		
+	}
+	
+	
+	
+	static int someFunction(int x){
+		return x;
+	}
+	
+	
 }
